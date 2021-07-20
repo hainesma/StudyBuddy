901db3b2752dedfbc788a37eaf7da8ec6d1e69eb
Andrew Klima <andrewklima7@gmail.com>
2021-07-20 14:29:07 -0500

    Favorite Controller HttpGet/HttpDelete/HttpPost add

---
+++ StudyBuddy/Context/StudyBuddyContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using StudyBuddy.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyBuddy
+{
+    public class StudyBuddyContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+
+        public StudyBuddyContext(DbContextOptions options)  : base(options){ }
+
+        public StudyBuddyContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=StudyBuddy;Trusted_Connection=True;");
+        }
+    }
+}
