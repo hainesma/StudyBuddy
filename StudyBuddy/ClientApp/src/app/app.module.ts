e2731aa56f527cf87f4e46bbf7b1fddb49f74662
Maggie Tamanini <82274835+teiggam@users.noreply.github.com>
2021-07-20 16:43:19 -0400

    Starting services

--- StudyBuddy/ClientApp/src/app/app.module.ts
+++ StudyBuddy/ClientApp/src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
+import { QuestionService } from './question.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      {path: 'question', component: QuestionComponent}
     ])
   ],
-  providers: [],
+  providers: [QuestionService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
