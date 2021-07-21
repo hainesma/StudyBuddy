829947fe4b2543dcd52383fae8cb16d89932dfde
Andrew Klima <andrewklima7@gmail.com>
2021-07-20 23:15:08 -0500

    Was able to get Question html to print questions, and added service and compotent to favorite having it print out list of favorites based on the userId given by user

--- StudyBuddy/ClientApp/src/app/question/question.component.ts
+++ StudyBuddy/ClientApp/src/app/question/question.component.ts
@@ -1,4 +1,4 @@
-﻿import { HttpClient } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Component, Inject } from "@angular/core";
 import { Questions} from "../questions";
 
