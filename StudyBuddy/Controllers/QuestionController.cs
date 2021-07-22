3aaeb8e5fd87fa631ccbb097bb8adc3eafa50bc8
Labden <nedbal.trent@gmail.com>
2021-07-22 15:47:49 -0400

    orca favicon

--- StudyBuddy/Controllers/QuestionController.cs
+++ StudyBuddy/Controllers/QuestionController.cs
@@ -13,9 +13,11 @@ namespace StudyBuddy.Controllers
     public class QuestionController : ControllerBase
     {
         StudyBuddyContext db = new StudyBuddyContext();
-
+        //getting data from the database using the httpget command
         [HttpGet]
         [Route("all")]
+
+        //Get questions method setting our List of questions qList to a new list and for each question q in our database of questions add q to the list and return the qList
         public List<Question> GetQuestions()
         {
             List<Question> qList = new List<Question>();
@@ -25,7 +27,7 @@ namespace StudyBuddy.Controllers
             }
             return qList;
         }
-
+        //posting the question to the database using the httppost command
         [HttpPost]
         [Route("add/{text}/{answer}")]
         public void AddQuestion(string text, string answer)
@@ -34,10 +36,10 @@ namespace StudyBuddy.Controllers
             db.Questions.Add(newQ);
             db.SaveChanges();
         }
-
+        //here we're getting the answer from the database via the userselected ID corresponding to the question
         [HttpGet]
         [Route("Id={userSelection}")]
-
+        //store our list of questions in qAns and call the GetQuestion method on it. got each question q in qAns if the id is equal to the user selection return q else return null;
         public Question getAnswer(int userSelection)
         {
             List<Question> qAns = GetQuestions();
